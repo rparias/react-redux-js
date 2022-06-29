98ed602ff37baaf913fd933124614e2d3f8f7abc
Ronald Arias <ariasron@hotmail.com>
2022-06-29 12:42:11 -0500

    refactor the app to work with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, addBy } from './features/counter/counterSlice'
 
 function App() {
   const counter = useSelector((state) => state.counter)
   const dispatch = useDispatch()
 
-  const increment = () => {
-    dispatch({ type: 'INCREMENT' })
-  }
-  const decrement = () => {
-    dispatch({ type: 'DECREMENT' })
-  }
-  const add10 = () => {
-    dispatch({ type: 'ADD10', payload: 10 })
-  }
-
   return (
     <div>
       <h1>Counter app</h1>
       <h2>{counter}</h2>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <button onClick={add10}>10+</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(addBy(10))}>10+</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,6 @@
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
-const reducer = (state = {counter: 0}, action) => {
-  switch(action.type) {
-    case 'INCREMENT':
-      return { counter: state.counter + 1 }
-    case 'DECREMENT':
-      return { counter: state.counter - 1 }
-    case 'ADD10':
-      return { counter: state.counter + action.payload }
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer)
-
-export default store
+export const store = configureStore({
+  reducer: counterReducer
+})
